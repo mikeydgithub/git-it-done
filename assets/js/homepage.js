9f201358b3d13c1442a0161f0af176a18a508d55
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-02-03 14:56:09 -0500

    get user repos and display repos added

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,16 +1,110 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+    // get value from input element
+
+    var username = nameInputEl.value.trim();
+    /*trim method removes whitespace from both ends of a string and returns a new string,
+    without modifying the original string.*/
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+  };
+
+
 //request any users repository
-var getUserRepos = function(user){
-    //format the github api url
-    var apiUrl = "https://api.github.com/users/" + user +"/repos";
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = 'https://api.github.com/users/' + user + '/repos';
 
     //make a request to the url
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            console.log(data);
-        });
-    });
+    fetch(apiUrl)
+        .then(function(response) {
+            //request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: GitHub User Not Found");
+            }
+        })
+        .catch(function(error) {
+            //notice this `.catch()` getting chained onto the end of the `.then()`
+            alert("Unable to connect to GitHub");
+        })
 };
 
+var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+      repoContainerEl.textContent = 'No repositories found.';
+      return;
+    }
+  
+    repoSearchTerm.textContent = searchTerm;
+  
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+      // format repo name
+      var repoName = repos[i].owner.login + '/' + repos[i].name;
+  
+      // create a container for each repo
+      var repoEl = document.createElement('div');
+      repoEl.classList = 'list-item flex-row justify-space-between align-center';
+  
+      // create a span element to hold repository name
+      var titleEl = document.createElement('span');
+      titleEl.textContent = repoName;
+  
+      // append to container
+      repoEl.appendChild(titleEl);
+  
+      // create a status element
+      var statusEl = document.createElement('span');
+      statusEl.classList = 'flex-row align-center';
+  
+      // check if current repo has issues or not
+      if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+      } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      }
+  
+      // append to container
+      repoEl.appendChild(statusEl);
+  
+      // append container to the dom
+      repoContainerEl.appendChild(repoEl);
+    }
+};
+
+
+// add event listeners to forms
+userFormEl.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
+
+
+
+
+
+//getUserRepos();
 
 
 //fetch api gathers resources
@@ -30,7 +124,6 @@ var getUserRepos = function(user){
     //var response = fetch("https:api.github.com/users/octocat/repos");
     //console.log(response); //returned promoise. Promises are more advanced callback functions.
 };
-
 getUserRepos();
 
 //Example of asynchronous behaviour. AJAX(Asynchronus JavaScripts and XML)
@@ -46,6 +139,10 @@ setTimeout(function(){
 
 //called immediately on page load
 console.log("hello 3");*/
+
+
+//curl commands allow you make request from the command line
+//alerts will block script from executing and therefore alerts are not asynchronous.
 
 /* Legacy Lore 
     Libraries like jQuery have south to make using XMLHttpRequest easier
